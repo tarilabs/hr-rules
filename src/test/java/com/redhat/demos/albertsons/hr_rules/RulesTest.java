c06e8c019c9483b0e808b9bb11b4773b719ddf9b
Rafael T. C. Soares <rafaelcba@gmail.com>
2020-06-30 16:13:07 -0400

    cleaning DRL syntax

--- src/test/java/com/redhat/demos/albertsons/hr_rules/RulesTest.java
+++ src/test/java/com/redhat/demos/albertsons/hr_rules/RulesTest.java
@@ -17,6 +17,10 @@ import com.opengamma.strata.basics.ReferenceData;
 import com.opengamma.strata.basics.date.HolidayCalendar;
 import com.opengamma.strata.basics.date.HolidayCalendarId;
 import com.opengamma.strata.basics.date.HolidayCalendarIds;
+import com.redhat.demos.albertsons.Employee;
+import com.redhat.demos.albertsons.HireDateReference;
+import com.redhat.demos.albertsons.WorkSheet;
+import com.redhat.demos.albertsons.WorkingHour;
 
 import org.junit.Test;
 import org.kie.api.runtime.KieSession;
@@ -28,17 +32,36 @@ public class RulesTest extends RulesBaseTest {
         KieSession kSession = createSession("stateful-session");
         assertNotNull(kSession);
 
+        HireDateReference hdr = new HireDateReference(LocalDate.of(2008, 5, 14), LocalDate.MAX, LocalDate.now());
 
-        // kSession.insert(loan);
-        // kSession.insert(loan2);
-        // kSession.insert(naturalDisasterEvent);
+        Employee employee1 = new Employee();
+        employee1.setId(1L);
+        employee1.setName("Rafael");
+        employee1.setHireDate(LocalDateTime.of(2014, 5, 18, 8, 0));
+        employee1.setBaseSalary(BigDecimal.valueOf(10000));
+        employee1.setRegularHourPayRate(BigDecimal.valueOf(25.00));
+
+        WorkSheet ws1 = new WorkSheet();
+        ws1.setId(10L);
+        ws1.setEmployeeId(employee1.getId());
+        ws1.addWorkingHour(new WorkingHour(LocalDateTime.of(2020, 6, 1, 8,  0)));
+        ws1.addWorkingHour(new WorkingHour(LocalDateTime.of(2020, 6, 1, 9,  0)));
+        ws1.addWorkingHour(new WorkingHour(LocalDateTime.of(2020, 6, 1, 10, 0)));
+
+        kSession.insert(hdr);
+        kSession.insert(employee1);
+        kSession.insert(ws1);
+
+        HolidayCalendarId holCalId = HolidayCalendarIds.NYSE;
+        HolidayCalendar holCal = holCalId.resolve(ReferenceData.standard());
+        System.out.println("Holiday Calendar name: " + holCal.getName());
+        kSession.setGlobal("holidaysCalendar", holCal);
+
         int fired = kSession.fireAllRules();
 
         System.out.println("1st Fire");
         System.out.println("Fired Rules: " + fired);
-        System.out.println("Results: ");
-        // System.out.println(loan);
-        // System.out.println(loan2);
+        System.out.println("Results: " + kSession.getObjects());
     }
 
     @Test
